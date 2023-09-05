44a9d61a9d59c0b5fa75fa2615eb7cba25ab9352
Raiyan Shaheen <raiyanshaheen@Raiyans-MacBook-Pro-2.local>
2023-09-05 10:12:26 +0100

    [added] colors page and route added

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -36,6 +36,16 @@ export function MainNav({
             href: `/${params.storeId}/sizes`,
             label: "Sizes",
             active: pathname === `/${params.storeId}/sizes`
+        },
+        {
+            href: `/${params.storeId}/colors`,
+            label: "Colors",
+            active: pathname === `/${params.storeId}/colors`
+        },
+        {
+            href: `/${params.storeId}/products`,
+            label: "Products",
+            active: pathname === `/${params.storeId}/products`
         }
     ]
 
